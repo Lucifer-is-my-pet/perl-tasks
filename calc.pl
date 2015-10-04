dec86133943ae8c2d389890b8e238ff92a7d86eb
Lucifer-is-my-pet <dianka-piratka@list.ru>
2015-10-05 03:18:39 +0500

    Дополнена программа с ОПН: теперь с учётом запятых

--- calc.pl
+++ calc.pl
@@ -1,13 +1,4 @@
-$len = @ARGV;
-$str = "";
-if ($len == 1) {
-	$answer = eval $ARGV[0];
-} else {
-	for $i(0 .. $len) {
-		$str .= $ARGV[$i];
-	}	
-	$answer = eval $str;
-}
-	
+$str = join("", @ARGV);
+$answer = eval $str;
 print($answer);
 	
--- rpn.pl
+++ rpn.pl
@@ -1,13 +1,22 @@
 $len = @ARGV;
+if ($len > 1) {
+	@arguments = @ARGV;
+}
+else {
+	@arguments = split (/,/, $ARGV[0]);
+}
+
+$len = @arguments;
 @arr = ();
 for $i(0 .. $len) {
-	if ($ARGV[$i] =~ m/\d+/) {
-		push @arr, $ARGV[$i];
+	if ($arguments[$i] =~ m/\d+/) {
+		push @arr, $arguments[$i];
 	} else {
 		$num2 = pop @arr;
 		$num1 = pop @arr;
-		$newNum = eval $num1 . $ARGV[$i] . $num2;
+		$newNum = eval $num1 . $arguments[$i] . $num2;
 		push @arr, $newNum;
 	}
 }
+
 print @arr[0];
