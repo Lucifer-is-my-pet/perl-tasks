7a363bb5caa99037bbb82bf94d84958d9700445b
Lucifer-is-my-pet <dianka-piratka@list.ru>
2015-12-22 02:34:12 +0500

    Вариант покороче и поприкольней

--- email.pl
+++ email.pl
@@ -7,47 +7,19 @@ Finding emails and names
 C<perl email.pl %filename%>
 
 =head1 USAGE
-finds emails and corresponding names and prints them
+finds emails and corresponding names in given hypertext file and prints them
 
 =head1 AUTHOR
 Guralnik Darya
-
-=head1 SUBROUTINES/METHODS
-C<findAllIndexes> finds indexes of all matches for given line and pattern
 =cut
 
 $\ = "\n";
-open my $text, '<', $ARGV[0] or die "Something's wrong with your file, $!";
+open my $file, '<', $ARGV[0] or die "Something's wrong with your file, $!";
 
-my %emails = ();
-while(<$text>) {
-	my $line = $_;
-    $line =~ m%mailto:%g;
-    my $mailtoInd = $-[0];
-    my @indexes = findAllIndexes($line, '"');
-    my $quoteInd = $indexes[-1];
-    my $emailInd = $mailtoInd + length("mailto:");
-    my $email = substr($line, $emailInd, $quoteInd - $emailInd);
-
-    @indexes = findAllIndexes($line, ">");
-    my $greaterInd = $indexes[0] + 1;
-    @indexes = findAllIndexes($line, "<");
-    my $lessInd = $indexes[-1];
-    my $name = substr($line, $greaterInd, $lessInd - $greaterInd);
-
-    $emails{$email} = $name;
-}
-
-while((my $key, my $value) = each %emails) {
+local $/;
+my $text = <$file>;
+my @info = ($text =~ /mailto:(.+)">(.+)</g) or next;
+my %emailsAndNames = @info;
+while(my ($key, $value) = each %emailsAndNames) {
     print "$key => $value";
 };
-
-sub findAllIndexes {
-    my $line = $_[0];
-    my $pattern = $_[1];
-    my @indexes = ();
-    while ($line =~ /$pattern/g) {
-        push @indexes, $-[0];
-    }
-    return @indexes;
-}
